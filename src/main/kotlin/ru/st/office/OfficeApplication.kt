5434719c8e3b3b5285c6876b9769dbab68cddc08
Sergey Tatarinov <serhii.tatarinov@gmail.com>
2020-07-17 11:09:14 +0300

    Fix path to migration

--- src/main/kotlin/ru/st/office/OfficeApplication.kt
+++ src/main/kotlin/ru/st/office/OfficeApplication.kt
@@ -10,7 +10,5 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 class OfficeApplication
 
 fun main(args: Array<String>) {
-    var getenv = System.getenv()
-    print(getenv)
     runApplication<OfficeApplication>(*args)
 }
